4ba61713ed3c3e3347f007daad07f07d94194a33
HuynhHoangThai <89508332+HuynhHoangThai@users.noreply.github.com>
2025-10-03 15:57:49 +0700

    test

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { assets } from '../../assets/assets';
-
+// import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
